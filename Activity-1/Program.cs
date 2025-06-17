ba1b2893bfea6b9a9919f7b6ac63a6a04f5253d8
iieiaoe <iieiaoe17@gmail.com>
2025-06-18 00:45:46 +0800

    Add project files.

---
+++ Activity-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GreetUser("John");
+        int result = Add(3, 5);
+        Console.WriteLine("Result: " + result);
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine("Hello, " + name);
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
